b5a0cb7a9275b18a8fde31c04934ac6a86b875c9
Kevin Rothenbühler-Alarcon <kevin.rothenbuhler@gmail.com>
2022-03-28 11:12:27 +0200

    Add responsive design

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ class Carousel {
      * @param {HTMLElement} element 
     */
     constructor(element) {
+        this.mobileMinWidth = 800
         this.element = element
 
         // Allow to affect default values to slidesTO Scroll and SlidesVisible of not all of them are present
@@ -12,10 +13,10 @@ class Carousel {
             loop: this.element.getAttribute("data-loop") === "true" || false
         }
         this.currentSlide = 0
+        this.isMobile = false
         this.moveCallbacks = []
         //get all the children from the element and store them in an array because we want the current state (we don't want it modified when we add the carousel)
         const children = [...element.children] 
-        
 
         // Create the main element and the container for the carousel (here it's for the example it would be easier to do it directly in html)
         this.root = this.createDivWithClass("carousel")
@@ -31,17 +32,18 @@ class Carousel {
 
         this.setStyle()
         this.createNavigation()
-        this.fireOnMoveCallback(0)
+        this.onWindowResize()
+        window.addEventListener("resize", this.onWindowResize.bind(this))
     }
 
     /**
      * Apply needed sizes to the carousel items
      */
     setStyle () {
-        const ratio = this.items.length / this.options.slidesVisible
+        const ratio = this.items.length / this.slidesToShow
         this.container.style.width = (ratio * 100) + "%"
         this.items.forEach(item => {
-            item.style.width = ((100 / this.options.slidesVisible) / ratio) + "%"
+            item.style.width = ((100 / this.slidesToShow) / ratio) + "%"
         })
     }
 
@@ -64,22 +66,23 @@ class Carousel {
             })
     
             this.addOnMoveCallback(index => {
-                if (this.currentSlide + this.options.slidesVisible >= this.items.length) {
+                if (this.currentSlide + this.slidesToShow >= this.items.length) {
                     nextButton.classList.add("hidden")
                 }
                 else {
                     nextButton.classList.remove("hidden")
                 }
             })
-        }      
+        } 
+        this.fireOnMoveCallback(this.currentSlide) // Fire the callbacks to show or hide the navigation arrow based on the new style     
     }
 
     next () {
-        this.goToSlide(this.currentSlide + this.options.slidesToScroll)
+        this.goToSlide(this.currentSlide + this.slidesToScroll)
     }
 
     previous() {
-        this.goToSlide(this.currentSlide - this.options.slidesToScroll)
+        this.goToSlide(this.currentSlide - this.slidesToScroll)
     }
 
     /**
@@ -88,8 +91,8 @@ class Carousel {
      */
     goToSlide(index) {
         if (index < 0 && this.currentSlide <= 0) {
-            index = this.items.length - this.options.slidesVisible
-        } else if (index >= this.items.length || (this.currentSlide + this.options.slidesVisible >= this.items.length && index > this.currentSlide)) {
+            index = this.items.length - this.slidesToShow
+        } else if (index >= this.items.length || (this.currentSlide + this.slidesToShow >= this.items.length && index > this.currentSlide)) {
             index = 0
         }
         const translateX = (-100 / this.items.length) * index
@@ -115,6 +118,30 @@ class Carousel {
         const div = document.createElement("div")
         div.classList.add(className)
         return div
+    }
+
+    /**
+     * @returns {number}
+     */
+    get slidesToScroll() {
+        return this.isMobile ? 1 : this.options.slidesToScroll
+    }
+
+    /**
+     * @returns {number}
+     */
+    get slidesToShow() {
+        return this.isMobile ? 1 : this.options.slidesVisible
+    }
+
+    onWindowResize() {
+        const oldValue = this.isMobile
+        this.isMobile = window.innerWidth < this.mobileMinWidth ? true : false
+        // Check that we've changed to mobile view
+        if (oldValue != this.isMobile) {
+            this.setStyle() // Reset the style for the carousel
+            this.fireOnMoveCallback(this.currentSlide) // Fire the callbacks to show or hide the navigation arrow based on the new style
+        }
     }
 }
 
